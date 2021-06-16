28ae761c7c4dac400d02beaeaf550d204b777d43
norway235 <73003968+norway235@users.noreply.github.com>
2021-06-15 19:28:16 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,51 +1,78 @@
-var dog, dogImage, happyDog, database, foodStock;
+//Create variables here
+var dog, happyDog, database, foodS, foodStock, foodCount;
 
-var gameState = "FoodInStock"
-
-function preload() {
-  dogImage = loadImage("Dog.png");
-  happyDog = loadImage("dogImg1.png");
+function preload()
+{
+  //load images here
+  dogImage = loadImage ("Images/dogImg.png"); 
+  happyDog = loadImage ("Images/dogImg1.png"); 
 }
 
 function setup() {
-	createCanvas(500, 500);
+	createCanvas (500, 500); 
   
-  dog = createSprite(250, 250, 40, 40);
-  dog.addImage(dogImage);
-  dog.scale = 0.3;
-  //database = firebase.database();
-  //foodStock = database.ref("Food");
-  //foodStock.on("value", readStock);
-  foodStock = 20;
+  dog = createSprite (250, 250, 40, 40); 
+  dog.addImage(dogImage); 
+  dog.scale = 0.3; createCanvas(800, 700);
+  
+  console.log("30");
+
+  database = firebase.database();
+  foodStock = database.ref('Food'); 
+  foodStock.on("value", readStock);
+   
+  console.log("40");
+
+  
 }
 
 
 function draw() {  
-  background(46, 139, 87);
+  background(46, 139, 87); 
 
-  if (keyDown(UP_ARROW) && gameState === "FoodInStock") {
-    //writeStock(foodS);
-    foodStock = foodStock -1;
+  if (keyWentDown(UP_ARROW)) {
+    writeStock(foodS);
     dog.addImage(happyDog);
-    setTimeout(() => { dog.addImage(dogImage); }, 1000); 
-  }
+  } 
 
-  if (foodStock <= 0) {
-    gameState = "FoodOutOfStock";
-  }
 
   drawSprites();
 
-  if (gameState === "FoodInStock") {
-    fill("white");
-    text("Press the Up-Arrow key to feed the dog", 150, 15);
-    fill("white");
-    text("Food Remaining: "+foodStock, 200, 100);
-  } else {
-    fill("white");
-    text("You are out of food!", 200, 100);
-  }
+  fill(255,255,254);
+  stroke("black");
+  text ("Food Remaining: " + foodS, 200, 100); 
+
+  
+  /*
+  if (foodS <= 0) { 
+    fill ("white"); 
+    text ("You are out of food!", 200, 100);
+  } else { 
+    fill ("white"); 
+    text ("Press the Up-Arrow key to feed the dog", 150, 15); 
+    fill ("white"); 
+    
+  } 
+  */
 }
 
+function readStock(data){
+  console.log("50");
+  foodS = data.val();
+  console.log(foodS);
+}
+
+function writeStock(foodCount) {
+  if (foodCount <= 0) {
+    foodCount = 0;
+  } else {
+    foodCount = foodCount -1;
+  }
+
+  database.ref("/").update({
+    Food: foodCount
+  })
+
+}
 
 
